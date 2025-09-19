a114799a330eb3f8790baa846822550fb04013d8
mdasif8393 <md.asif.8393@gmail.com>
2025-09-19 17:20:18 +0600

    51-6 Exploring dynamic routes and catch-all routes

---
+++ src/components/Counter.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1 className="text-center text-3xl text-red-500">{count}</h1>
+      <div className="flex justify-center mt-2">
+        <button
+          onClick={() => setCount((prev) => prev - 1)}
+          type="button"
+          className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        >
+          Decrease
+        </button>
+        <button
+          onClick={() => setCount(0)}
+          type="button"
+          className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        >
+          Reset
+        </button>
+        <button
+          onClick={() => setCount((prev) => prev + 1)}
+          type="button"
+          className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        >
+          Increase
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
